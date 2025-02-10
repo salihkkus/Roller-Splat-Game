ea9b954044ec9436c0663f11d3cd8b795f89eff2
salihkarakus <sskarakussalih77@gmail.com>
2025-02-10 09:14:37 +0300

    ball script update

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    private Vector2 firstPos;
+    private Vector2 secondPos;
+    private Vector2 currentPos;
+    public float speed;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Swipe();
+    }
+
+    private void Swipe()
+    {
+       if(Input.GetMouseButtonDown(0))
+       {
+         firstPos = new Vector2(Input.MousePosition.x , Input.MousePosition.x);
+       }
+
+        if(Input.GetMouseButtonUp(0))
+       {
+         secondPos = new Vector2(Input.MousePosition.x , Input.MousePosition.x);
+       }
+
+       currentPos = new Vector2(secondPos.x - firstPos.x , secondPos.y - firstPos.y);
+
+       currentPos.Normalize();
+
+       if(currentPos.y < 0 && currentPos.x < 0.5f && currentPos.x > 0.5f ) //back
+       {
+          rb.velocity = Vector3.back * speed;
+       }
+       else if(currentPos.y > 0  && currentPos.x < 0.5f && currentPos.x > 0.5f) //forward
+       {
+        rb.velocity = Vector3.forward * speed;
+       }
+       else if(currentPos.x < 0 && currentPos.y < 0.5f && currentPos.y > 0.5f) // left
+       {
+        rb.velocity = Vector3.left * speed;
+       }
+       else if(currentPos.x > 0 && currentPos.y < 0.5f && currentPos.y > 0.5f) // right
+       {
+        rb.velocity = Vector3.right * speed;
+       }
+
+    }
+    
+   
+
+}
